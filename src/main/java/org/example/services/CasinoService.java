bdb229261859fe6bc2250ac8e9dddb7998b10c20
Oksana Dvornitska <userdvornicka@gmail.com>
2023-09-03 12:45:05 +0300

    fixed problem with makeBet not working properly(deposit didnt decrease)

--- src/main/java/org/example/services/CasinoService.java
+++ src/main/java/org/example/services/CasinoService.java
@@ -11,52 +11,72 @@ public class CasinoService implements CasinoServiceI {
     private Integer deposit;
     @Value("${cellsAmount}")
     private Integer cellsAmount;
+
     public Integer getDeposit() {
         return deposit;
     }
 
     @Override
     public String makeBet(Integer betSize, TypesOfBets typesOfBets, Integer cellNumber) {
-        StringBuilder res = new StringBuilder("вы проиграли");
+        StringBuilder res = new StringBuilder("");
         Integer randomNumber = (int) (Math.random() * cellsAmount);
         int win = 0;
-        switch (typesOfBets){
-            case ZERO ->{
-                if(randomNumber == 0){
-                    win = betSize*(cellsAmount-2);
-                    deposit += win;
-                    res.isEmpty();
-                    res.append("вы выиграли ");
-                    res.append(win);
+        if (deposit >= betSize) {
+            switch (typesOfBets) {
+                case ZERO -> {
+                    if (randomNumber == 0) {
+                        win = betSize * (cellsAmount - 2);
+                        deposit += win;
+                        res.isEmpty();
+                        res.append("вы выиграли ");
+                        res.append(win);
+                    } else {
+                        res.append("вы проиграли");
+                        deposit -= betSize;
+                    }
+                }
+                case NUMBER -> {
+                    if (randomNumber.equals(cellNumber)) {
+                        win = betSize * (cellsAmount - 2);
+                        deposit += win;
+                        res.isEmpty();
+                        res.append("вы выиграли ");
+                        res.append(win);
+                    } else {
+                        res.append("вы проиграли");
+                        deposit -= betSize;
+                    }
+                }
+                case RED, EVEN -> {
+                    if (randomNumber % 2 == 0 && randomNumber != 0) {
+                        win = betSize;
+                        deposit += win;
+                        res.isEmpty();
+                        res.append("вы выиграли ");
+                        res.append(win);
+                    } else {
+                        res.append("вы проиграли");
+                        deposit -= betSize;
+                    }
+                }
+                case BLACK, ODD -> {
+                    if (randomNumber % 2 == 1) {
+                        win = betSize;
+                        deposit += win;
+                        res.isEmpty();
+                        res.append("вы выиграли ");
+                        res.append(win);
+                    } else {
+                        res.append("вы проиграли");
+                        deposit -= betSize;
+                    }
                 }
             }
-            case NUMBER -> {
-                if(randomNumber.equals(cellNumber)){
-                    win = betSize*(cellsAmount-2);
-                    deposit += win;
-                    res.isEmpty();
-                    res.append("вы выиграли ");
-                    res.append(win);
-                }
-            }
-            case RED, EVEN -> {
-                if(randomNumber%2 ==0 && randomNumber != 0){
-                    win = betSize;
-                    deposit += win;
-                    res.isEmpty();
-                    res.append("вы выиграли ");
-                    res.append(win);
-                }
-            }
-            case BLACK, ODD ->{
-                if(randomNumber%2 ==1 ){
-                    win = betSize;
-                    deposit += win;
-                    res.isEmpty();
-                    res.append("вы выиграли ");
-                    res.append(win);
-                }
-            }
+        }else {
+            res.append("на вашем балансе недостаточно средств");
+        }
+        if (deposit == 0) {
+            res.append(", НУ ТЫ ЛОХ");
         }
         return res.toString();
 
